04835ddb6d02ff45a57d15fb916ddfffcff55cc1
Surafel <surafelyigezuu@gmail.com>
2025-07-08 09:52:53 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+
+////////////////////////////////////////////////////////////
+/////// File
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+//Non-blocking, asynchronous way
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+
+////////////////////////////////////////////////////////////
+/////// Server
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('Hello from the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('Hello from the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'content-type': 'text/html',
+      'my-own-header': 'hello world',
+    });
+    res.end('<h2>Page not found!</h2>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
