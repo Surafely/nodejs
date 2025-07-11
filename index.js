9359c9eb8e72e78a675a1f31202bab140016f7bb
Surafel <surafelyigezuu@gmail.com>
2025-07-11 09:06:17 +0300

    Replacing the placeholders with the content

--- index.js
+++ index.js
@@ -27,20 +27,65 @@ const url = require('url');
 
 ////////////////////////////////////////////////////////////
 /////// Server
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/templete-card.html`,
+  'utf-8'
+);
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/templete-overview.html`,
+  'utf-8'
+);
+const dataProduct = fs.readFileSync(
+  `${__dirname}/templates/templete-product.html`,
+  'utf-8'
+);
+
+const replaceTemp = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+
+  return output;
+};
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObject = JSON.parse(data);
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  // Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('Hello from the OVERVIEW');
-  } else if (pathName === '/product') {
+    res.writeHead(200, {
+      'content-type': 'text/html',
+    });
+
+    const cardHtml = dataObj.map((el) => replaceTemp(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARD%}', cardHtml);
+    res.end(output);
+  }
+
+  //Product page
+  else if (pathName === '/product') {
     res.end('Hello from the PRODUCT');
-  } else if (pathName === '/api') {
+  }
+
+  //API
+  else if (pathName === '/api') {
     res.writeHead(200, { 'content-type': 'application/json' });
     res.end(data);
-  } else {
+  }
+
+  //Not found
+  else {
     res.writeHead(404, {
       'content-type': 'text/html',
       'my-own-header': 'hello world',
