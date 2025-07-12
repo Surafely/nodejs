7e4d03fe01c9a95dc46d4468006c2bca7d14a555
Surafel <surafelyigezuu@gmail.com>
2025-07-12 15:55:02 +0300

    Importing and exporting modules

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemp = require('./modules/replaceTemplate');
+
 ////////////////////////////////////////////////////////////
 /////// File
 
@@ -40,22 +42,6 @@ const dataProduct = fs.readFileSync(
   `${__dirname}/templates/templete-product.html`,
   'utf-8'
 );
-
-const replaceTemp = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-
-  return output;
-};
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
